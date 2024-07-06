ef85d92ce4e3cff8695838f0b3f80d794c322de1
André Firmino <andrecnl@gmail.com>
2024-07-06 18:27:33 -0300

    Insert Knex ( migrations ) and zod in project

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,30 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('tb_transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 100.25,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
+})
+
+app.get('/transactions', async () => {
+  const transactions = await knex('tb_transactions').select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
